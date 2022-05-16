3341062b95f2ae7878394cd5f6c03822ffbcb6e8
TimotejkoPantak <timopantakgit@gmail.com>
2022-05-16 17:39:30 +0200

    Add basic routing and create todos component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TOdosModule } from 'src/app/todos/todos.modules';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    TOdosModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
